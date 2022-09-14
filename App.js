5ee9abfb7f771c5844d8b99033d60a38ec593d2d
shyamtiwari02062001 <shyam02062001@gmail.com>
2022-09-14 08:05:30 +0530

    Added the input box with styling needed

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
 const App = () => {
-  return (
-    <View>
-      <Text>Shyam</Text>
-    </View>
-  );
+  return <StartGameScreen />;
 };
 export default App;
+const styles = StyleSheet.create({});
