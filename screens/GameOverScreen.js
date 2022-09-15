493fcdfed574a3f84659d41735b2b559bb3551f3
shyamtiwari02062001 <shyam02062001@gmail.com>
2022-09-15 08:18:21 +0530

    Fixed the GameEndScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,31 +1,56 @@
 import React from 'react';
-import {View, Image, StyleSheet, Text, Dimensions} from 'react-native';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  ScrollView,
+} from 'react-native';
 import PrimaryButton from '../components/ui/primaryButton';
 import Title from '../components/ui/title';
 import Colors from '../constants/color';
 const GameOverScreen = ({roundsNumber, userNumber, onStartNewGame}) => {
+  const {width, height} = useWindowDimensions();
+  let image = 300;
+  if (width < 380) {
+    image = 150;
+  }
+  if (height < 400) {
+    image = 80;
+  }
+  const imageStyle = {
+    width: image,
+    height: image,
+    borderRadius: image / 2,
+  };
   return (
-    <View style={styles.rootConatiner}>
-      <Title text={'Game OVER'} />
-      <View style={styles.imageConatiner}>
-        <Image
-          style={styles.image}
-          source={require('../assets/images/success.png')}
-        />
+    <ScrollView contentContainerStyle={styles.scroll}>
+      <View style={styles.rootConatiner}>
+        <Title text={'Game OVER'} />
+        <View style={[styles.imageConatiner, imageStyle]}>
+          <Image
+            style={styles.image}
+            source={require('../assets/images/success.png')}
+          />
+        </View>
+        <Text style={styles.text}>
+          Your phone needed
+          <Text style={styles.highlightText}> {roundsNumber} </Text>
+          rounds to guess the number
+          <Text style={styles.highlightText}> {userNumber} </Text>
+        </Text>
+        <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
       </View>
-      <Text style={styles.text}>
-        Your phone needed
-        <Text style={styles.highlightText}> {roundsNumber} </Text>
-        rounds to guess the number
-        <Text style={styles.highlightText}> {userNumber} </Text>
-      </Text>
-      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
-    </View>
+    </ScrollView>
   );
 };
 export default GameOverScreen;
-const width = Dimensions.get('window').width;
 const styles = StyleSheet.create({
+  scroll: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   rootConatiner: {
     flex: 1,
     padding: 24,
@@ -33,9 +58,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   imageConatiner: {
-    width: width < 300 ? 150 : 300,
-    height: width < 300 ? 150 : 300,
-    borderRadius: width < 300 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: 'hidden',
